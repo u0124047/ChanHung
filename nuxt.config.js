d76ff9bc3a855e36345427d2080a2df9a04bdfbf
Kathy Kuo <dudoo.kathy@gmail.com>
2021-09-09 08:58:36 +0800

    調整seo

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,11 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { name: 'Keywords', content: '郭庭如,塑膠廢料,資源回收,廢塑料回收,台南塑膠,KathyKuo,PE,PE塑膠,LDPE,LDPE塑膠,LDPE塑膠回收,HDPE,HDPE塑膠,HDPE塑膠回收,再生料,PE再生料,台南再生料,展虹,展虹有限,展虹有限公司,展虹塑膠,睿虹'},
       { name: 'Description', content: '台南展虹有限公司｜PE再製料｜PE塑膠膜｜PE料買賣' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:description', content: '台南展虹有限公司｜PE再製料｜PE塑膠膜｜PE料買賣' },
+      { property: 'og:url', content: 'https://u0124047.github.io/InteriorDesignWeb' },
+      { property: 'fb:app_id', content: '223595172974610' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
