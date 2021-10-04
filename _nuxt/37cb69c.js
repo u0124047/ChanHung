8aed522b9fffddf47bc7a3c73fce1c5bbe1727b9
Kathy Kuo <dudoo.kathy@gmail.com>
2021-10-04 09:30:57 +0800

    e5d8277

---
+++ _nuxt/37cb69c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{114:function(e,o,n){"use strict";var c=n(1),t=n(157),_=n.n(t);c.a.use(_.a,{page_id:"223595172974610",theme_color:"#6699cc",locale:"zh_TW",logged_out_greeting:"嗨~有問題可以私訊我喔！",logged_in_greeting:"您好，很高興為您服務"})},160:function(e,o,n){n(161),e.exports=n(162)}},[[160,7,1,8]]]);
