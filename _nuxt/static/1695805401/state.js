28cb023f6e289e2a78613bba497a08f22b6e020d
Kathy Kuo <u0124047@gmail.com>
2023-09-27 17:03:27 +0800

    d392c4a

---
+++ _nuxt/static/1695805401/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FChanHung\u002F_nuxt\u002Fstatic\u002F1695805401",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FChanHung\u002F",assetsPath:"\u002FChanHung\u002F_nuxt\u002F",cdnURL:a}}}}(null));
